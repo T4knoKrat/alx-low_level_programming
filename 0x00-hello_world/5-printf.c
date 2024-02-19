08f9cdad94026e50beaa86b062a0837be7819a8f
Elon <bonventureelon@gmail.com>
2024-02-19 11:51:12 +0300

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main-entry point
+ * Return:Always 0 (success)
+ */
+#include<stdio.h>
+int main(void)
+{
+	printf("with proper programming but the outcome is aa piece of art,");
+	return (0);
+}
