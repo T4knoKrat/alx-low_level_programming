ca4911673cadaebd185f8ac511f69cf14dc5f158
Elon <bonventureelon@gmail.com>
2024-02-20 00:05:59 +0300

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main-Entry point
+* Return: 0(success)
+*
+*/
+int main(void)
+{	
+	char i;
+	for(i = 'a';i <= 'z';i++)
+	{
+	putchar(i);
+	}
+	return (0);
+
+}
