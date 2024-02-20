7204beb09253f1bd875969f25becec3c9c73afef
Elon <bonventureelon@gmail.com>
2024-02-20 21:59:43 +0300

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - entry point
+ * Return:0(Success)
+ */
+int main(void)
+{
+	int i;
+	for(i = 'z';i >= 'a';i--)
+	{
+	putchar( i);
+	}
+	putchar('\n');
+	
+	return(0);
+
+}
