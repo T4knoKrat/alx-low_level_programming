6d81039bafd2dfd5a2a24c0c4127cd02a9fa9067
Elon <bonventureelon@gmail.com>
2024-02-20 07:38:14 +0300

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* entry-Entry point
+* Return:0(success)
+*/
+int main(void)
+{	
+	char i;
+	for(i = 'a'; i <= 'z'; i++)
+	{
+	putchar(i);
+	}
+	for(i = 'A';i <= 'Z';i++)
+	{
+	putchar(i);
+	}
+
+	return(0);
+}
