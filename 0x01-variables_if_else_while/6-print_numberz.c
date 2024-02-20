8134c0bd50c78adda7bae0b1f476d8a30f18e32a
Elon <bonventureelon@gmail.com>
2024-02-20 12:50:19 +0300

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *main -Entry point
+ *Return:0 (success)
+*/
+
+int main(void)
+{	
+
+	int i;
+	for(i = 0;i <= 9;i++)
+	{
+	putchar(48 + i);
+	}
+	putchar('\n');
+	return (0);
+
+}
