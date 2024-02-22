186243f1c30ea623d9481a95a86d55f076741cd3
Elon <bonventureelon@gmail.com>
2024-02-22 17:56:21 +0300

    1-main.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main - check the code
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{	int i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+		_putchar('\n');
+	return (0);
+}
