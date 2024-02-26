92206befa63c01bf9dbd28bd6077c9a16ae536f8
Elon <bonventureelon@gmail.com>
2024-02-26 15:08:18 +0300

    abs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ *print_sign - function to check the sign of a number
+ *@n: - the character to be entered
+ *Returns:1 when it is positive
+ *On zeros; 0
+ *On negative1
+ */
+int print_sign(int n)
+{	
+	if (n > 0 )
+	{
+		if (n == 0)
+		{	return (0);
+			_putchar('0' + 48);
+		}
+		else
+		{	return (1);
+			_putchar('+');
+		}
+	}
+	else
+	{	return (-1);
+		_putchar('-');
+	}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs-function to calculate  the absolute value
+ * @r:-character to be checked
+ * Return:0 Always succes
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+	return (-r);
+	}
+	else
+	return (r);
+}
