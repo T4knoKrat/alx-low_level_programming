57abe17a2771a57dc336c33df77fff4d1e577b7a
Elon <bonventureelon@gmail.com>
2024-02-27 15:10:50 +0300

    9x9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include"main.h"
+/**
+ * times_table-function for x9
+ * Return:0 always
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			int product = x * y;
+			int pcount = 0;
+			pcount++;
+
+			if ((product / 10) == 0)
+			{
+			
+				_putchar((product + '0'));
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+
+			}
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+			_putchar('\n');
+	}
+}
