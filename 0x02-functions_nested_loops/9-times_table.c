a0545c6363d70df923b431a96787358600d78ff3
Elon <bonventureelon@gmail.com>
2024-02-27 15:50:26 +0300

    9x9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,21 +14,40 @@ void times_table(void)
 		{
 			int product = x * y;
 
-			if ((product / 10) == 0)
+			if (y != 9)
 			{
-				_putchar((product + '0'));
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 
+				if ((product / 10) == 0)
+				{
+					_putchar((product + '0'));
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+
+				}
+				else
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			else
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
+				if ((product / 10) == 0)
+				{
+					_putchar(' ');
+					_putchar((product + '0'));
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+					_putchar(' ');
+				}
+
+
 		}
 			_putchar('\n');
 	}
