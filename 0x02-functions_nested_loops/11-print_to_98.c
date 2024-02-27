fe6a2dc9de353d61aa3f16da48e81951776faebc
Elon <bonventureelon@gmail.com>
2024-02-27 18:46:10 +0300

    go to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include"main.h"
+/**
+ * print_to_98-function to display numbers
+ * @n: integer to be entered
+ * Return:void
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+			n--;
+		}
+	}
+	printf("\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,6 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+#include<stdlib.h>
+#include<stdio.h>
 #endif 
