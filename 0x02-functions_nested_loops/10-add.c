58d2d5ae43149d2b441915dd11c8fbb190c36b8f
Elon <bonventureelon@gmail.com>
2024-02-27 17:42:46 +0300

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -9,15 +9,7 @@ int add(int a, int b)
 {
 	int c = a + b;
 
-	if ((c / 10) == 0)
-	{
-		_putchar(c + '0');
-	}
-	else
-	{
-		_putchar(c/10);
-		_putchar(c%10);
-	}
+	_putchar(c % 10 );
 	return (c);
 }
 
