e3e1271133f7864bf2a530ee481b10e2ceaa3b92
Elon <bonventureelon@gmail.com>
2024-03-06 16:46:08 +0300

    arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_rev - print reverse
+ * @s:string
+ * return:0
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	for (i = len; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+
+}
