ae9f679e3d3805da504a5182e2cb0dd5c8dc5f9a
T4knoKrat <bonventureelon@gmail.com>
2024-03-11 16:02:08 +0300

    puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,11 +12,11 @@ void puts_half(char *str)
 	len = 0;
 
 	for (a = 0; str[a] != '\0'; a++)
-	longi++;
+	len++;
 
-	n = (longi / 2);
-	if ((longi % 2) == 1)
-	n = ((longi + 1) / 2);
+	n = (len / 2);
+	if ((len % 2) == 1)
+	n = ((len + 1) / 2);
 	for (a = n; str[a] != '\0'; a++)
 	_putchar(str[a]);
 	_putchar('\n');
