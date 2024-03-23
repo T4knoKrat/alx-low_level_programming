b0c422dced5b3a6fb6139a0aa0b1ae7f90c61848
T4knoKrat <bonventureelon@gmail.com>
2024-03-24 01:02:42 +0300

    updated strcpy function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+*char *_strcpy -function to convert string to integer
+*@dest:string
+*@src:string
+*Return:dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+	int x = 0;
+
+	while (*(src + len) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < len; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[len] = '\0';
+	return (dest);
+}
+{
