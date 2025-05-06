97851830fc5d10f0e33ecdeb760b229db465b5f1
T4knoKrat <bonventureelon@gmail.com>
2025-05-06 15:05:01 +0300

    updated 9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
-#include<stdlib.h>
 /**
- * main -Entry point
+ * main-prints all possible combinations
+ * of single digit numbers in ascending order
  * Return:0(success)
  */
 int main(void)
-{	int i;
-	for (i = 48; i < 58; i++)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
-		if (i != 57)
-		{
-		putchar(',');
-		putchar(0 + 32);
-		}
+		putchar(i + 48);
 	}
-	putchar(0 + 10);
-	return (0);
+	putchar('\n');
 }
