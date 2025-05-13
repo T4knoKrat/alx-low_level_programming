dc6e532b6e2f0a12c893ea85a31862d627a1d0c1
T4knoKrat <bonventureelon@gmail.com>
2025-05-13 23:30:05 +0300

    updated alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_alphabet_x10-prints lowercase alphabet x10
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
