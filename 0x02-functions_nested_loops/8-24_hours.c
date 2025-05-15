c680c0765fd9c1143fd440d6f5cd1304cae24958
T4knoKrat <bonventureelon@gmail.com>
2025-05-15 18:49:14 +0300

    john bauer updated

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include"main.h"
+/**
+ * jack_bauer-prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		if ((i == 2 && j == 3) && (j == 5 && l == 9))
+		{
+		}
+		else
+		{
+			for (j = 0; j <= 9; j++)
+			{
+				for (k = 0; k <= 5; k++)
+				{
+					for (l = 0; l <= 9; l++)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+
+}
