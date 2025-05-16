9236ec778e6fa1226b3290c1aa48bc6807270be8
T4knoKrat <bonventureelon@gmail.com>
2025-05-16 12:18:29 +0300

    9-multiple updated

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include"main.h"
+/**
+ * times_table-prints 9 * 0 timetable
+ */
+void times_table(void)
+{
+	int i,j,k,l,m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = 0;
+			k = i * j;
+			if (k < 10)
+			{
+				_putchar(k + '0');
+				if (j != 9)
+				_putchar(',');
+			}
+			else
+			{	l = k / 10;
+				m = k % 10;
+				_putchar(l + '0');
+				_putchar(m + '0');
+				if (j != 9)
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+	
+}
