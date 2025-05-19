4d40de4cc11cc34f98e77ba3c8134f9d28fa9460
T4knoKrat <bonventureelon@gmail.com>
2025-05-20 02:00:03 +0300

    updated 50 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,15 +6,15 @@
 int main(void)
 {
 	int c;
-	long int j,k,l;
+	long int j, k, l;
 
 	j = 1;
 	k = 2;
-	printf("%ld,%ld", j, k);
-	for (c = 0; c <= 48; c++)
+	printf("%ld, %ld", j, k);
+	for (c = 0; c <= 47; c++)
 	{
 		l = j + k;
-		printf(",%ld", l);
+		printf(", %ld", l);
 		j = k;
 		k = l;
 	}
