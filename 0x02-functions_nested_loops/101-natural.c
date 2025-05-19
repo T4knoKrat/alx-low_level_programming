550f7092f10f4440d75a0f052c6e65ac25975b3b
T4knoKrat <bonventureelon@gmail.com>
2025-05-20 00:39:18 +0300

    add sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
 /**
  * main-prints sum of multiples of 3 and 5
- * @j:sum
- * Return:sum
+ * Return:0(success)
  */
 int main(void)
 {
-	int i, j;
+	int i, j = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
-			j = 0;
 			j += i;
 		}
 	}
