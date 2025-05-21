7ef470eb34c4b4f587e0cf636abed1c3838acd91
T4knoKrat <bonventureelon@gmail.com>
2025-05-21 23:52:16 +0300

    prints numbers

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _isupper - checks if parameter is an uppercase character.
  * @c: input character.
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,6 +6,7 @@
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 48; i <= 57; i++)
 	{
 		_putchar(i);
