5670169da29e88d9d652465a023ec101a3c534bc
T4knoKrat <bonventureelon@gmail.com>
2025-05-22 22:49:35 +0300

    updated diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,19 +6,16 @@
  */
 void print_diagonal(int n)
 {
-	int i = 0;
-
+	int i;
+	
 	if (n <= 0)
-	{
-	}
 	if (n > 0)
 	{
-		while (i <= n - 1)
+		for (i = 0; i <= n - 1; i++)
 		{
 			_putchar(' ');
-			i++;
 		}
 		_putchar(92);
 	}
 	_putchar('\n');
-}
+}	
