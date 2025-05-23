fc9095fdae232149c8b7bc8011c012a6ec515942
T4knoKrat <bonventureelon@gmail.com>
2025-05-24 02:51:50 +0300

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,20 +9,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			if ((i % 3 == 0) && (i % 5 == 0))
-			{
-				printf("FizzBuzz ");
-			}
-			if (i % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			if (i % 5 == 0)
-			{
-				printf("Buzz ");
-			}
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
 		}
 		else
 		{
@@ -30,4 +27,5 @@ int main(void)
 		}
 	}
 	printf("\n");
+	return (0);
 }
