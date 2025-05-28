7b80458a8e2ddf2f94f78f1b76e6769f2366e0ad
T4knoKrat <bonventureelon@gmail.com>
2025-05-28 22:41:34 +0300

    updated string reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * rev_string - Reverse a string
  * @s: Input string
  * Return: String in reverse
  */
-
 void rev_string(char *s)
 {
 	char rev = s[0];
@@ -20,5 +18,5 @@ void rev_string(char *s)
 		rev = s[i];
 		s[i] = s[counter];
 		s[counter] = rev;
-											}
+	}
 }
