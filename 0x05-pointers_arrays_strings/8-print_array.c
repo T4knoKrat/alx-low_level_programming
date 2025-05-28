8253773e0aa3b647911ea1df7c44ff220d55c766
T4knoKrat <bonventureelon@gmail.com>
2025-05-29 01:09:07 +0300

    updated print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,9 @@ void print_array(int *a, int n)
 	{
 		printf("%d, ", a[i]);
 	}
-		if (i == (n - 1))
-		{
-			printf("%d", a[n - 1]);
-		}
-			printf("\n");
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+	printf("\n");
 }
