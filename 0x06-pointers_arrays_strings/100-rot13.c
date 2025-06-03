ef30ba74e66cad5b2b61cbd1beea4f98891d0d0e
T4knoKrat <bonventureelon@gmail.com>
2025-06-04 00:41:36 +0300

    updated rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * rot13-encodes using rot13
+ * @s:string to be encoded
+ * Return:s
+ */
+char *rot13(char *s)
+{
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char replace[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = replace[j];
+			}
+		}
+	}
+	return (s);
+}
