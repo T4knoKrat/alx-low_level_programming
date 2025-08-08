4d5c327b95538eb5433c6a9101a26b012c8b1bed
T4knoKrat <bonventureelon@gmail.com>
2025-08-08 22:11:18 +0300

    updated argc...

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main-displays name of program
+ * @argc: first parameter
+ * @argv: second parameter
+ * Return:0 success
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
