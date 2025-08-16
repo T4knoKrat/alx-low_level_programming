9658014dc573db0eb2d5b09a0aeb500225f0c89d
T4knoKrat <bonventureelon@gmail.com>
2025-08-17 01:41:37 +0300

    updated argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+/**
+ * main-prints number of arguments
+ * @argc:argument to main
+ * @argv:argument to main
+ * Return:0 success
+ */
 int main(int argc, char **argv)
 {
 	(void)argv;
 	printf("%d\n", argc - 1);
-	return(0);
+	return (0);
 }
